f99fe2a97502630abb1d66c99f17e2c4807a01e7
Sangki Jeon <sangki1298@gmail.com>
2025-03-11 00:20:02 +0900

    수정

--- proj_apps/core/views.py
+++ proj_apps/core/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from proj_apps.psychotests.models import Test, Category
 
 def home(request):
-    # 임시로 간단한 응답 반환
-    return render(request, 'core/home.html', {})
+    # 최신 테스트
+    recent_tests = Test.objects.all().order_by('-created_at')[:6]
+    
+    # 인기 테스트
+    popular_tests = Test.objects.all().order_by('-views')[:12]
+    
+    # 추천 테스트 (Featured)
+    featured_tests = Test.objects.filter(featured=True)[:5]
+    
+    # 카테고리
+    categories = Category.objects.all().order_by('order')[:4]
+    
+    context = {
+        'recent_tests': recent_tests,
+        'popular_tests': popular_tests,
+        'featured_tests': featured_tests,
+        'categories': categories,
+    }
+    return render(request, 'core/home.html', context)
 
 def about(request):
     return render(request, 'core/about.html', {})
+
+def contact(request):
+    return render(request, 'core/contact.html', {})
